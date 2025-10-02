fa1825ce6c1ae2abae8c7fc9db27b9c9415b44d1
sewalewsetotaw <sewalewsetotaw2023@gmail.com>
2025-10-02 03:56:27 +0300

    add Routing

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="p-4 bg-gray-800 text-white flex gap-4">
+      <Link href="/home">Home</Link>
+      <Link href="/about">About</Link>
+    </header>
+  );
+}
