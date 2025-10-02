d02385ecf7b7bbca6f7c06a70d32db52ac694f53
sewalewsetotaw <sewalewsetotaw2023@gmail.com>
2025-10-02 04:02:48 +0300

    Create a Reusable Card Component

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,10 @@
+import { CardProps } from "@/interfaces";
+
+export default function Card({ title, content }: CardProps) {
+  return (
+    <div className="border rounded-lg p-4 shadow-md">
+      <h2 className="text-xl font-semibold">{title}</h2>
+      <p>{content}</p>
+    </div>
+  );
+}
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,3 +1,10 @@
+import Card from "@/components/common/Card";
 export default function HomePage() {
-  return <h1 className="text-2xl font-bold">This is the Home Page</h1>;
+  return (
+    <div className="space-y-4 p-6">
+      <h1 className="text-2xl font-bold">This is the Home Page</h1>
+      <Card title="Card 1" content="This is the first card." />
+      <Card title="Card 2" content="This is another card with different content." />
+    </div>
+  )
 }
